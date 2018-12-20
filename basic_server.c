e757abc0b92693025d6a6f4decce4d93729a9245
djiang2001 <djiang2@stuy.edu>
2018-12-19 22:59:27 -0500

    init2

--- basic_server.c
+++ basic_server.c
@@ -1,10 +1,38 @@
 #include "pipe_networking.h"
+#include <signal.h>
+
+static void sighandler(int signo){
+  if(signo == SIGINT){
+    remove("wkp");
+    exit(0);
+  }
+}
+
+int mychange(char* data){
+      while(*data){
+	strcat(data,"y");
+	data++;
+      }
+      return 0;
+}
 
 
 int main() {
+  signal(SIGINT, sighandler);
 
   int to_client;
   int from_client;
 
-  from_client = server_handshake( &to_client );
+  while(1){
+    from_client = server_handshake( &to_client );
+    char msg[BUFFER_SIZE];
+
+    while(read(from_client,msg,BUFFER_SIZE)){
+      msg[strlen(msg) - 1] = '\0';
+      printf("Server received: %s \n",msg);
+      mychange(msg);
+      write(to_client,msg,BUFFER_SIZE);
+      printf("Server message: %s \n", msg);
+    }
+  }
 }
--- client.c
+++ client.c
@@ -8,11 +8,18 @@ int main() {
 
   from_server = client_handshake( &to_server );
 
+  char input[BUFFER_SIZE];
+  char output[BUFFER_SIZE];
+
   while(1){
   printf("Input message: \n");
 
-  char input[BUFFER_SIZE];
-  fgets(input,BUFFER_SIZE,stdin);
 
+  fgets(input,BUFFER_SIZE,stdin);
+  input[strlen(input) -1] = '\0';
+
+  write(to_server,input,BUFFER_SIZE);
+  read(from_server, output, BUFFER_SIZE);
+  printf("Server response: %s \n", output);
   }
 }
